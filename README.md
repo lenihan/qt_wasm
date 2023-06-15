8771915ba01233fbbde98ac7a545033194ea2875
David Lenihan <davidlenihan@outlook.com>
2023-06-14 22:32:53 -0500

    updated script to build qt with wasm in peer repo dir

--- README.md
+++ README.md
@@ -1,2 +1,235 @@
-# qt_wasm
-Test Qt Wasm support
+# qt_wasm - build/run qt native and web apps
+
+- [qt\_wasm - build/run qt native and web apps](#qt_wasm---buildrun-qt-native-and-web-apps)
+  - [Features](#features)
+  - [Goals](#goals)
+  - [Prerequisites](#prerequisites)
+    - [Disk Space](#disk-space)
+    - [Apps](#apps)
+  - [Setup](#setup)
+  - [Build/Run/Debug](#buildrundebug)
+  - [Clean](#clean)
+  - [Create A CMake Project](#create-a-cmake-project)
+  - [CMake Tips](#cmake-tips)
+    - [Helpful Documentation](#helpful-documentation)
+    - [ALL\_BUILD and ZERO\_CHECK](#all_build-and-zero_check)
+    - [Copy files](#copy-files)
+    - [CMake Comment](#cmake-comment)
+    - [Print Message/Variable](#print-messagevariable)
+    - [Show All CMake Variables](#show-all-cmake-variables)
+  - [Qt Tips](#qt-tips)
+    - [Qt tools for Visual Studio](#qt-tools-for-visual-studio)
+  - [OpenSceneGraph Tips](#openscenegraph-tips)
+    - [OpenSceneGraph GitHub Discussions](#openscenegraph-github-discussions)
+    - [OpenSceneGraph Google Group](#openscenegraph-google-group)
+    - [OpenSceneGraph Books](#openscenegraph-books)
+  - [Visual Studio Code](#visual-studio-code)
+    - [Environment](#environment)
+    - [To run CMake configure](#to-run-cmake-configure)
+
+## Features
+- F12 to look up symbols (both local and third party)
+- Builds all third party dependencies locally
+- Debug symbols including third party
+- Source for third party
+- Debug via one mouse click in VS Code (builds code, launches debug executable with gdb for active project)
+- Cross platform: works on Linux, Mac, Windows
+- Built around VS Code, but supports any IDE (including - Visual Studio on Windows)
+- Builds/runs locally (after setup, can disconnect from internet and continue developing)
+- Fast compilation via precompiled headers
+- Fast linking via visibility/dllexport (only export symbols designated as public)
+
+## Goals
+
+- Simple - Err on the side of simple solutions that new users will easily understand
+- Fast - Want to be able to develop fast, debug fast, iterate fast, run fast
+- Easy - Things should "just work" or be easy to figure out
+- Crossplatform - Windows, Linux, Mac
+- Multiple copies of repo
+- Run locally
+
+## Prerequisites
+
+### Disk Space
+
+TODO
+
+### Apps
+
+- ![Windows](./resources/icons/windows_16x16.png) Windows
+  1. Install *Visual Studio 2022 Community*
+     1. Download and open [vs_Community.exe](https://aka.ms/vs/17/release/vs_community.exe)
+     2. Select *"Desktop development with C++"* Workload
+     3. Click *"Install"* button
+     * NOTE: To verify proper install, you should be able to run `cl` from the "Developer Command Prompt for VS 2022" without error.
+  2. Open terminal and install `git`, `pwsh`, `cmake`, `perl`, `ninja`, `python3`, and `code`
+    ```pwsh
+    'Git.Git', '9MZ1SNWT0N5D', 'cmake', 'StrawberryPerl.StrawberryPerl', 'XP9KHM4BK9FZ7Q', 'Ninja-build.Ninja', '9NRWMJP3717K'  | % {winget install $_ --accept-source-agreements --accept-package-agreements}
+    ```
+  3. Close terminal (this terminal must be restarted to access newly installed apps)
+     
+- ![Linux](./resources/icons/linux_16x16.png) Linux
+  1. Open terminal and install `pwsh`, `git`, and `code`
+    ```pwsh
+    sudo snap install powershell --channel=lts/stable --classic; sudo apt-get install git --yes; sudo snap install code --classic
+    ```
+
+- ![MacOS](./resources/icons/macos_16x16.png) Mac
+  
+  1. SIP (System Integrity Protection) must be disabled to run setup script. Follow [these](https://developer.apple.com/documentation/security/disabling_and_enabling_system_integrity_protection) instructions.
+  2. Open terminal and install `brew` and `PowerShell`
+  ```shell
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; brew install --cask powershell
+  ```
+
+## Setup
+
+![Windows](./resources/icons/windows_16x16.png) Windows/![Linux](./resources/icons/linux_16x16.png) Linux/![MacOS](./resources/icons/macos_16x16.png) Mac
+1. Open terminal and clone repo, run setup script (~1-2 hours)
+  ```pwsh
+  pwsh -Command "git clone https://github.com/lenihan/tron.git $HOME/repos/tron; ~/repos/tron/scripts/setup.ps1"
+  ```
+
+## Build/Run/Debug
+
+- Visual Studio Code (![Windows](./resources/icons/windows_16x16.png) Windows/![Linux](./resources/icons/linux_16x16.png) Linux/![MacOS](./resources/icons/macos_16x16.png) Mac)
+  1. Open terminal and launch Visual Studio Code with repo folder
+    ```pwsh
+    pwsh -Command "code $HOME/repos/tron"
+    ```
+  2. Install Workspace Recommended extensions
+     - View > Extensions > Filter Extensions... > Recommended
+     - Click "Install" on each extension
+  3. To launch a target, click the "play" icon the status bar (bottom)
+  4. Select a Kit
+     - Windows: `Visual Studio Community 2022 Release - amd64`
+     - Linux: `GCC 9.4.0 x86_64-linux-gnu`
+     - Mac: `Clang 14.0.0 x86_64-apple-darwin 21.6.0`
+  5. Select a launch target
+     - `hello_cmake` to test CMake 
+     - `hello_osg` to test OpenSceneGraph
+     - `hello_qt` to test Qt  
+  6. To launch debugger, click the "debug" icon in the status bar (bottom) 
+- Visual Studio 2022 (![Windows](./resources/icons/windows_16x16.png) Windows)
+  1. Open terminal and generate .sln file, launch in Visual Studio
+    ```pwsh
+    pwsh -Command "cmake -S ~/repos/tron -B ~/repos/tron/build; ~/repos/tron/build/tron.sln"
+    ``` 
+  2. Solution Explorer > Right click on project > Set as Startup Project
+     - `hello_cmake` to test CMake 
+     - `hello_osg` to test OpenSceneGraph
+     - `hello_qt` to test Qt  
+  3. Debug > Start Debugging 
+
+## Clean
+
+All output (CMake, compiler, linker, etc.) go to *./build* directory. To clean up, delete *./build*.
+
+## Create A CMake Project 
+
+See `~/repos/tron/src/hello/hello_cmake/CMakeLists.txt` for a simple example.
+
+1. Create `CMakeLists.txt` in project directory under `~/repos/tron/src`
+2. First line sets project name: `project(<PROJECT_NAME>)`
+3. Add `project_common()` to get settings that apply to all projects.
+4. Add source code to compile via [add_executable](https://cmake.org/cmake/help/latest/command/add_executable.html)
+5. Link libraries with [target_link_libraries](https://cmake.org/cmake/help/latest/command/target_link_libraries.html)
+6. Add project directory to  `~/repos/tron/CmakeLists.txt` with [add_subdirectory](https://cmake.org/cmake/help/latest/command/add_subdirectory.html)
+7. Generate build files in *./build*
+  ```pwsh
+  pwsh -Command 'cmake -S ~/repos/tron -B ~/repos/tron/build'
+  ```
+
+
+## CMake Tips
+
+### Helpful Documentation
+
+- [CMake Language](https://cmake.org/cmake/help/latest/manual/cmake-language.7.html)
+- [CMake Commands](https://cmake.org/cmake/help/latest/manual/cmake-commands.7.html)
+- [CMake Variables](https://cmake.org/cmake/help/latest/manual/cmake-variables.7.html)
+- [CMake Generators](https://cmake.org/cmake/help/latest/manual/cmake-generators.7.html)
+- [CMake Qt](https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html)
+
+### ALL_BUILD and ZERO_CHECK
+
+CMake creates two predefined projects
+
+- ALL_BUILD - Build all projects
+- ZERO_CHECK - Check to see if any files are out of date, and re-run CMake if needed.
+
+ALL_BUILD has a dependency on all projects. When you build ALL_BUILD, you build everything.
+
+ZERO_CHECK looks at the timestamp on `generate.stamp` files to determine if CMake needs to re-run. For example, if you add a new source file to a 'CMakeLists.txt` file, ZERO_CHECK will detect this and regenerate build files. All projects have a dependency on ZERO_CHECK to ensure all build files are up to date before building.
+
+### Copy files
+
+Use `configure_file` to copy files. The copy happens during CMake build file generation. See `src/sandbox/QTreeView` for an example.
+
+### CMake Comment
+
+[CMake Comment Documentation](https://cmake.org/cmake/help/v3.1/manual/cmake-language.7.html#comments)
+
+- Everything after `#` is a comment
+  - Like C++ `//`
+- Everthing in betwee `#[[` and `]]` is a comment
+  - Like C++ `/* ... */`
+
+### Print Message/Variable
+
+[CMake's Message Documentation](https://cmake.org/cmake/help/latest/command/message.html)
+
+```cmake
+message("Hello")
+message(${PROJECT_NAME})
+```
+
+### Show All CMake Variables
+
+Add this to a `CMakeLists.txt` to see all CMake variables
+
+```cmake
+get_cmake_property(_variableNames VARIABLES)
+list (SORT _variableNames)
+foreach (_variableName ${_variableNames})
+    message(STATUS "${_variableName}=${${_variableName}}")
+endforeach()
+```
+
+## Qt Tips
+
+### Qt tools for Visual Studio
+
+* Install [Qt Visual Studio Tools](https://marketplace.visualstudio.com/items?itemName=TheQtCompany.QtVisualStudioTools2022)
+  * Adds debugging extensions for Qt data types
+
+## OpenSceneGraph Tips
+
+### OpenSceneGraph GitHub Discussions
+
+Where OpenSceneGraph Q&A happens today. This is active.
+
+<https://github.com/openscenegraph/OpenSceneGraph/discussions>
+
+### OpenSceneGraph Google Group
+
+Email forum with 18,000+ conversations. This is where most questions are posted/answered. No longer active, but contains lots of Questions and answers.
+
+<https://groups.google.com/g/osg-users>
+
+### OpenSceneGraph Books
+
+- [OpenSceneGraph 3.0: Beginner's Guide](https://www.amazon.com/OpenSceneGraph-3-0-Beginners-Rui-Wang-ebook/dp/B0057761U4)
+- [OpenSceneGraph 3 Cookbook](https://www.amazon.com/OpenSceneGraph-3-Cookbook-Rui-Wang/dp/184951688X)
+
+## Visual Studio Code
+
+### Environment
+
+Required environment variables are embedded in vs code workspace. You do not need to modify your environment to run/debug apps within vs code.
+
+### To run CMake configure
+
+This should happen automatically as needed (for example, when you click on Build).
+
+To run manually: Ctrl+Shift+P > CMake: Configure
---
+++ scripts/setup.ps1
@@ -0,0 +1,252 @@
+# Setup prerequisites, build environment, third_party directory, .env file for running apps.
+#Requires -Version 7
+
+$ROOT_DIR = Resolve-Path $PSScriptRoot/..
+$REPOS_DIR = Resolve-Path $ROOT_DIR/..
+$QT_DIR = Join-Path $REPOS_DIR qt6
+$VCPKG_DIR = Join-Path $REPOS_DIR vcpkg
+
+if ($IsWindows) {$TRIPLET = "x64-windows"}  
+if ($IsLinux)   {$TRIPLET = "x64-linux"}    
+if ($IsMacOS)   {$TRIPLET = "x64-osx"}     
+
+function echo_command($cmd) {
+    Write-Host $cmd -ForegroundColor Cyan
+    Invoke-Expression $cmd
+}
+
+function setup_prerequisites {
+    Write-Host "Setup prerequisites..." -ForegroundColor Green
+    if ($IsWindows) {
+        # verify access to required apps
+        $required_apps = 
+            "git", 
+            "pwsh", 
+            "cmake", 
+            "perl", 
+            "code", 
+            "python3",
+            "ninja",
+            "$env:ProgramFiles\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1" 
+        $all_commands_found = $true
+        foreach ($ra in $required_apps) {
+            $found_command = Get-Command $ra -ErrorAction SilentlyContinue
+            if (!$found_command) {
+                $all_commands_found = $false
+                Write-Host "Could not find command: $ra" -ForegroundColor Red
+            }
+        }
+        if (!$all_commands_found) {
+            Write-Host "Cannot continue without access to required commands." -ForegroundColor Red
+            exit
+        }
+    }
+    elseif ($IsLinux) {
+        $installed_packages = apt list --installed 2> $null
+        $upgradeable_packages = apt list --upgradeable 2> $null
+        function is_package_installed($pkg) {if ($installed_packages | Select-String "^$pkg/") {$true} else {$false}}
+        function is_package_upgradeable($pkg) {if ($upgradeable_packages | Select-String "^$pkg/") {$true} else {$false}}
+        $packages = "curl",                 # for vcpkg
+                    "cmake",                # for vcpkg
+                    "build-essential",      # for vcpkg: gcc, g++, make, C standard lib, dev tools                    
+                    "bison",                # for gettext  (used by osg)
+                    "python3-distutils",    # for fontconfig (used by osg)
+                    "libgl1-mesa-dev",      # for osg "Could not find OpenGL"
+                    "libtool",              # for osg
+
+                    # for qt5: Some learned from https://github.com/microsoft/vcpkg/blob/master/scripts/azure-pipelines/linux/provision-image.sh
+                    "libglu1-mesa-dev",     # for freeglut (used by qt5)
+                    "libxi-dev",            # for angle (used by qt5)
+                    "libxext-dev",          # for angle (used by qt5)
+                    "autoconf",             # for icu (used by qt5)
+                    "autoconf-archive",     # for icu (used by qt5)
+                    "libx11-dev",
+                    "libxkbcommon-x11-dev",                    
+                    "libxext-dev",
+                    "libxfixes-dev",
+                    "libxrender-dev",
+                    "libxcb1-dev",
+                    "libx11-xcb-dev",
+                    "libxcb-glx0-dev",
+                    "libxcb-util0-dev",
+                    "libxkbcommon-dev",
+                    "libxcb-keysyms1-dev",
+                    "libxcb-image0-dev",
+                    "libxcb-shm0-dev",
+                    "libxcb-icccm4-dev",
+                    "libxcb-sync-dev",
+                    "libxcb-xfixes0-dev",
+                    "libxcb-shape0-dev",
+                    "libxcb-randr0-dev",
+                    "libxcb-render-util0-dev",
+                    "libxcb-xinerama0-dev",
+                    "libxcb-xkb-dev",
+                    "libxcb-xinput-dev"
+
+        $ran_apt_update = $false
+        foreach ($pkg in $packages) {
+            $installed = is_package_installed($pkg)
+            if (!$installed) {
+                if (!$ran_apt_update) {
+                    echo_command "sudo apt update"
+                    $ran_apt_update = $true
+                }
+                echo_command "sudo apt install -y $pkg"
+            }
+        }
+        foreach ($pkg in $packages) {
+            $upgradeable = is_package_upgradeable($pkg)
+            if ($upgradeable) {
+                if (!$ran_apt_update) {
+                    echo_command "sudo apt update"
+                    $ran_apt_update = $true
+                }
+                echo_command "sudo apt upgrade -y $pkg"
+            }
+        }
+    }
+    elseif ($IsMacOS) {
+        # Unable to build qt5 (moc can't find libbz2d.1.0.dylib) because SIP (System Integrity Protection) won't pass
+        # DYLD_LIBRARY_PATH to subprocesses.
+        # Fix: Disable SIP
+        # If SIP is enabled, exit
+        $SIP_enabled = (csrutil status) -match "enabled."
+        if ($SIP_enabled) {
+            Write-Host "SIP (System Integrity Protection) must be disabled to build third party libraries." -ForegroundColor Red
+            Write-Host "To disable, follow instructions here:" -ForegroundColor Red
+            Write-Host "https://developer.apple.com/documentation/security/disabling_and_enabling_system_integrity_protection" -ForegroundColor Red
+            exit           
+        }
+
+        echo_command "brew install --cask visual-studio-code"
+        echo_command "brew install autoconf"
+        echo_command "brew install automake"
+        echo_command "brew install libtool"
+        echo_command "brew install nasm"
+        echo_command "brew install cmake"
+        echo_command "brew install autoconf-archive"
+        echo_command "brew install gettext"
+    }
+}
+
+function setup_build_environment {
+    Write-Host "Setup build environment..." -ForegroundColor Green
+    if ($IsWindows) {
+        Push-Location .  # Next line can put us in ~/source/repos, fix that with Pop-Location
+        & "$env:ProgramFiles\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64
+        Pop-Location
+    }
+}
+
+function setup_third_party {
+    Write-Host "Setup third party..." -ForegroundColor Green
+
+    # qt build with wasm support
+    Write-Host "Git clone Qt..." -ForegroundColor Green
+    #
+    # clone qt, init repo
+    cd $REPOS_DIR
+    $QT_DIR_NAME = "qt6"
+    git clone git://code.qt.io/qt/qt5.git $QT_DIR_NAME
+    $QT_DIR = Join-Path $REPOS_DIR $QT_DIR_NAME
+    cd $QT_DIR
+    git switch v6.5.1 --detach # from `git tag`
+    perl ./init-repository
+    # NOTE: Assuming setup_build_environment() already called
+    # 
+    # build native qt
+    Write-Host "Build native Qt..." -ForegroundColor Green
+    if ($IsWindows) {$os = "windows"}
+    if ($IsLinux)   {$os = "linux"}
+    if ($IsMacOS)   {$os = "osx"}
+    $BUILD_NATIVE_DIR = Join-Path $QT_DIR "build-$os"
+    mkdir $BUILD_NATIVE_DIR
+    cd $BUILD_NATIVE_DIR
+    # configure qt, create ninja build files via cmake, set install to ./install dir
+    ../configure -prefix ./install
+    # build/install qt
+    cmake --build . --parallel
+    cmake --install .
+    #
+    # Install emsdk
+    Write-Host "Install/activate emsdk..." -ForegroundColor Green
+    cd $REPOS_DIR
+    # git clone https://github.com/emscripten-core/emsdk.git
+    $EMSDK_DIR = Join-Path $REPOS_DIR "emsdk"
+    cd $EMSDK_DIR
+    # Required emscripten version for Qt 3.6.1 is Emscripten 3.1.25 (from above https://doc.qt.io/qt-6/wasm.html)
+    ./emsdk install 3.1.25 
+    ./emsdk activate 3.1.25
+    . ./emsdk_env.ps1
+    #
+    # build wasm qt (from https://doc.qt.io/qt-6/wasm.html)
+    Write-Host "Build wasm Qt..." -ForegroundColor Green
+    cd $QT_DIR
+    $BUILD_WASM_DIR = Join-Path $QT_DIR "build-wasm"
+    mkdir $BUILD_WASM_DIR
+    cd $BUILD_WASM_DIR
+    ../configure -qt-host-path "$BUILD_NATIVE_DIR/install" -platform wasm-emscripten –prefix ./install 
+    cmake --build . --parallel 
+    cmake --install . 
+}
+
+function setup_environment_file {
+    Write-Host "Setup environment file..." -ForegroundColor Green
+    $ENV_FILE = Join-Path $ROOT_DIR .env
+    Write-Host "Generate environment file $ENV_FILE for running apps"  -ForegroundColor Green
+
+    # Output .env
+    if ($IsWindows) {
+@"
+PATH=$env:PATH
+VSCMD_ARG_TGT_ARCH=$env:VSCMD_ARG_TGT_ARCH
+"@ | Set-Content $ENV_FILE
+    }
+    if ($IsLinux) {
+@"
+PATH=$env:PATH
+"@ | Set-Content $ENV_FILE
+    }
+    if ($IsMacOS) {
+@"
+PATH=$env:PATH
+"@ | Set-Content $ENV_FILE
+    }    
+
+    # Add environment variables to vs code workspace
+    $VS_CODE_WORKSPACE_SETTINGS_PATH = Join-Path $ROOT_DIR .vscode settings.json
+    if (Test-Path $VS_CODE_WORKSPACE_SETTINGS_PATH) {
+        $settings = Get-Content $VS_CODE_WORKSPACE_SETTINGS_PATH | ConvertFrom-Json
+    } else {
+        New-Item -ItemType File $VS_CODE_WORKSPACE_SETTINGS_PATH -Force | Out-Null
+        $settings = New-Object -TypeName PSObject
+    }
+    if ($IsWindows) {$os = "windows"}
+    if ($IsLinux)   {$os = "linux"}
+    if ($IsMacOS)   {$os = "osx"}
+    $env = @{}
+    Get-Content $ENV_FILE | ForEach-Object {
+        $name, $value = $_ -split '='
+        $env += @{$name = $value}
+    }   
+
+    # store settings
+    $terminal_integrated_env_os = $env
+    $cmake_environment          = $env
+    $files_associations         = @{"**/include/**" = "cpp"}
+    $search_useIgnoreFiles      = $false
+    $search_exclude             = @{"**/build" = $true; "third_party/vcpkg" = $true}
+
+    # save settings
+    $settings | Add-Member -MemberType NoteProperty -Name "terminal.integrated.env.$os" -Value $terminal_integrated_env_os -Force
+    $settings | Add-Member -MemberType NoteProperty -Name "cmake.environment"           -Value $cmake_environment          -Force
+    $settings | Add-Member -MemberType NoteProperty -Name "files.associations"          -Value $files_associations         -Force
+    $settings | Add-Member -MemberType NoteProperty -Name "search.useIgnoreFiles"       -Value $search_useIgnoreFiles      -Force
+    $settings | Add-Member -MemberType NoteProperty -Name "search.exclude"              -Value $search_exclude             -Force
+    $settings | ConvertTo-Json | Set-Content $VS_CODE_WORKSPACE_SETTINGS_PATH
+}
+
+setup_prerequisites
+setup_build_environment
+setup_third_party
+setup_environment_file
